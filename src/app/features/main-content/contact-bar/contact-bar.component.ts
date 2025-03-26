613eac7598a9fdce365e2c19fb6e72785524cb8c
SusanneRenken <renken.susanne@gmail.com>
2025-03-26 18:52:19 +0100

    folder structure added

---
+++ src/app/features/main-content/contact-bar/contact-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-bar',
+  imports: [],
+  templateUrl: './contact-bar.component.html',
+  styleUrl: './contact-bar.component.scss'
+})
+export class ContactBarComponent {
+
+}

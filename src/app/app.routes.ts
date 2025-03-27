90d7fe770ee2c1ad5e02eb0736d3ff8b5fd04a77
SusanneRenken <renken.susanne@gmail.com>
2025-03-27 08:16:51 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AccessComponent } from './features/access/access.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AccessComponent },
+];

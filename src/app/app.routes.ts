c1b3cf5e49d849d8634a5e3a368950bc96ac9c74
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-27 15:48:53 +0100

    merge main to feature/access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { MainPageComponent } from './features/access/main-page/main-page.component';
-
-export const routes: Routes = [
-  { path: '', component: MainPageComponent, data: { active: 'Main', scrollTo: '' } },
-=======
 import { AccessComponent } from './features/access/access.component';
 import { MainContentComponent } from './features/main-content/main-content.component';
 
@@ -12,5 +6,4 @@ export const routes: Routes = [
   { path: '', component: AccessComponent },
   { path: 'home', component: MainContentComponent },
   { path: 'home/:userId', component: MainContentComponent },
->>>>>>> main
 ];

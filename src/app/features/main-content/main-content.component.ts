12cba337ba1b8427d404f6bb924496b81d814605
SusanneRenken <renken.susanne@gmail.com>
2025-03-27 08:28:06 +0100

    structure of the main-content-component

--- src/app/features/main-content/main-content.component.ts
+++ src/app/features/main-content/main-content.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactBarComponent } from './contact-bar/contact-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageAreaComponent } from './message-area/message-area.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [ContactBarComponent, HeaderComponent, MessageAreaComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

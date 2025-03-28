90c65a9893e95ee88b2a1ad926931d91ffd58c29
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-28 15:21:24 +0100

    add component-switcher service

--- src/app/features/access/access.component.ts
+++ src/app/features/access/access.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { NgComponentOutlet } from '@angular/common';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 @Component({
   selector: 'app-access',
@@ -19,6 +21,12 @@ export class AccessComponent {
         break;
       case 'signin':
         this.currentComponent = CreateAccountComponent;
+        break;
+      case 'imprit':
+        this.currentComponent = ImprintComponent;
+        break;
+      case 'privacy':
+        this.currentComponent = PrivacyComponent;
         break;
       default:
         this.currentComponent = LoginComponent;

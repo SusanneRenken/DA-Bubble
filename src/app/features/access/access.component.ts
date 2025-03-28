debb0c79e1a215e883b8f782c66916b48e8b48be
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-28 01:02:31 +0100

    disign login

--- src/app/features/access/access.component.ts
+++ src/app/features/access/access.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-access',
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './access.component.html',
   styleUrl: './access.component.scss'
 })
 export class AccessComponent {
+  currentComponent = LoginComponent;
 
+  setComponent(componentName: string): void {
+    switch(componentName) {
+      case 'login':
+        this.currentComponent = LoginComponent;
+        break;
+      case 'signin':
+        this.currentComponent = CreateAccountComponent;
+        break;
+      default:
+        this.currentComponent = LoginComponent;
+    }
+  }
 }
